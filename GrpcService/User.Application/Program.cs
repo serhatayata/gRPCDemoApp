8a45efea9e7928f675c6d8eed500cce48a70ca8b
Serhat <srhtayata@gmail.com>
2023-05-28 20:11:36 +0300

    update for console app

---
+++ GrpcService/User.Application/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using ProductOfferGrpcService.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5135");
+var client = new UserOfferService.UserOfferServiceClient(channel);
+
+var serverReply = client.GetUserOfferList(new EmptyRequestArg { });
+Console.WriteLine(serverReply);
+
+Console.ReadLine();
